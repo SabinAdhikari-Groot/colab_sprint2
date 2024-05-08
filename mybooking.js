dad6afa4d055e3e2953c0cd98416bb3dd45bef91
SabinAdhikari-10 <127823232+SabinAdhikari-Groot@users.noreply.github.com>
2024-05-09 00:39:03 +0545

    Add files via upload

---
+++ mybooking.js
@@ -0,0 +1,25 @@
+function backToHomepage() {
+    window.location.href = "nhomepage.html";
+}
+
+function pay() {
+    window.location.href = "process_payment.php";
+}
+
+function cancelBooking() {
+    if (confirm("Are you sure you want to cancel this booking?")) {
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status === 200) {
+                    location.reload();
+                } else {
+                    alert("Error cancelling booking.");
+                }
+            }
+        };
+        xhr.open("POST", "cancel_booking.php", true);
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhr.send();
+    }
+}
